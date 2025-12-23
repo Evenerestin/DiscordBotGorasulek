996b3c613713da913c0bf9369790325801299c1b
Alicja Bieryt <alicjabieryt.dev@gmail.com>
2025-12-23 01:39:03 +0100

    push

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -21,7 +21,7 @@ const PORT = 3000;
 app.use(express.json({ limit: "10mb" }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://gorasul.pl"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
